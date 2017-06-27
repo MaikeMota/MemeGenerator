f002d0dcc36140a2f7e02e74c58048926884a6c5
Maike Mota <maike_henrique@hotmail.com>
2017-06-26 23:37:48 -0300

    Model for Meme
     - Overrides toJson to serelialize only src from Image
     - Static method parser to parse data to Image

---
+++ assets/js/Meme.js
@@ -0,0 +1,28 @@
+class Meme {
+
+    constructor(image, topLineText, bottomLine) {
+        this.id = new Date().getTime();
+        this.image = image;
+        this.topLineText = topLineText;
+        this.bottomLineText = bottomLineText;
+    }
+
+    toJSON() {
+        return JSON.stringify({
+            id: this.id,
+            image: this.image.src,
+            topLineText: this.topLineText,
+            bottomLineText: this.bottomLineText
+        });
+    }
+
+    static parser(key, value) {
+        if (key === 'image') {
+            let image = new Image();
+            image.src = value;
+            return image;
+        } else {
+            return value;
+        }
+    }
+}
